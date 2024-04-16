825e9781c5e2f1e56bf7ad9ae7641d80aaf60b26
Sam Morris <sam@robots.org.uk>
2024-04-16 11:48:58 +0100

    Document gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,11 @@
-# pylint: skip-file
+# This file is used to configure gunicorn when running from the container
+# image. As a result, a user can override any of these properties by providing
+# additional arguments to the container.
 
 bind = ["[::]:9770"]
 
 accesslog = "-"
 
 wsgi_app = "rsync_net_exporter:create_app()"
+
+# pylint: skip-file
