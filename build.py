c42f6e0df0726b1a002907a11fcc31aed1b1362e
Sam Morris <sam@robots.org.uk>
2024-04-19 10:23:15 +0100

    build.conf refactor
    Split out creating and mounting containers into two separate context
    managers and added logging.

--- build.py
+++ build.py
@@ -1,17 +1,20 @@
 import contextlib
 import json
+from logging import basicConfig, getLogger
 from pathlib import Path
 import shutil
 import subprocess
 import sys
 
 
+LOGGER = getLogger(__name__)
+
 RELEASEVER = "9"
 PYTHON_SUFFIX = "3.11"
 
 
 def main(argv):
-    subprocess.run(
+    run(
         [
             "buildah",
             "build",
@@ -25,57 +28,54 @@ def main(argv):
 
     with buildah_from(
         ["--pull", f"registry.access.redhat.com/ubi{RELEASEVER}/ubi-micro"]
-    ) as (
-        micro_ctr,
-        micro_mountpoint,
-    ):
+    ) as production_ctr:
 
-        subprocess.run(
-            [
-                "dnf",
-                "-y",
-                "--noplugins",
-                f"--installroot={micro_mountpoint}",
-                f"--releasever={RELEASEVER}",
-                "--nodocs",
-                "--setopt=install_weak_deps=0",
-                "install",
-                "python3.11",
-            ],
-            check=True,
-        )
-        subprocess.run(
-            [
-                "dnf",
-                "-y",
-                "--noplugins",
-                f"--installroot={micro_mountpoint}",
-                f"--releasever={RELEASEVER}",
-                "clean",
-                "all",
-            ],
-            check=True,
-        )
+        with buildah_mount(production_ctr) as production_mnt:
 
-        shutil.rmtree(micro_mountpoint / f"usr/share/python{PYTHON_SUFFIX}-wheels")
+            (production_mnt / "opt/app-root").mkdir(parents=True)
 
-        Path(micro_mountpoint / "opt/app-root").mkdir(parents=True)
+            with buildah_from(["rsync.net-exporter-builder"]) as builder_ctr:
+                with buildah_mount(builder_ctr) as builder_mnt:
+                    shutil.copytree(
+                        builder_mnt / "opt/app-root/venv",
+                        production_mnt / "opt/app-root/venv",
+                        symlinks=True,
+                    )
 
-        with buildah_from(["rsync.net-exporter-builder"]) as (
-            builder_ctr,
-            builder_mountpoint,
-        ):
-            shutil.copytree(
-                builder_mountpoint / "opt/app-root/venv",
-                micro_mountpoint / "opt/app-root/venv",
-                symlinks=True,
+            shutil.copy(
+                "gunicorn.conf.py", production_mnt / "opt/app-root/gunicorn.conf.py"
+            )
+            run(
+                [
+                    "dnf",
+                    "-y",
+                    "--noplugins",
+                    f"--installroot={production_mnt}",
+                    f"--releasever={RELEASEVER}",
+                    "--nodocs",
+                    "--setopt=install_weak_deps=0",
+                    "install",
+                    "python3.11",
+                ],
+                check=True,
             )
 
-        shutil.copy(
-            "gunicorn.conf.py", micro_mountpoint / "opt/app-root/gunicorn.conf.py"
-        )
+            run(
+                [
+                    "dnf",
+                    "-y",
+                    "--noplugins",
+                    f"--installroot={production_mnt}",
+                    f"--releasever={RELEASEVER}",
+                    "clean",
+                    "all",
+                ],
+                check=True,
+            )
 
-        subprocess.run(
+            shutil.rmtree(production_mnt / f"usr/share/python{PYTHON_SUFFIX}-wheels")
+
+        run(
             [
                 "buildah",
                 "config",
@@ -84,41 +84,46 @@ def main(argv):
                 "--port=9770/tcp",
                 "--user=1001:0",
                 "--workingdir=/opt/app-root",
-                f"--entrypoint={json.dumps(['venv/bin/python', '-m', 'gunicorn'])}",
-                micro_ctr,
+                "--entrypoint=" + json.dumps(["venv/bin/python", "-m", "gunicorn"]),
+                production_ctr,
             ],
             check=True,
         )
 
-        subprocess.run(
-            ["buildah", "commit", micro_ctr, "rsync.net-exporter"], check=True
-        )
+        run(["buildah", "commit", production_ctr, "rsync.net-exporter"], check=True)
 
     return 0
 
 
 @contextlib.contextmanager
 def buildah_from(args):
-    p1 = subprocess.run(
-        ["buildah", "from", *args], text=True, stdout=subprocess.PIPE, check=True
-    )
+    p1 = run(["buildah", "from", *args], text=True, stdout=subprocess.PIPE, check=True)
     ctr = p1.stdout.strip()
     assert ctr
     try:
-        p2 = subprocess.run(
-            ["buildah", "mount", ctr], text=True, stdout=subprocess.PIPE, check=True
-        )
-        mountpoint = p2.stdout.strip()
-        assert mountpoint
-        try:
-            yield ctr, Path(mountpoint)
-        finally:
-            subprocess.run(["buildah", "unmount", ctr], check=True)
+        yield ctr
     finally:
-        subprocess.run(["buildah", "rm", ctr], check=True)
+        run(["buildah", "rm", ctr], check=True)
+
+
+@contextlib.contextmanager
+def buildah_mount(ctr):
+    p2 = run(["buildah", "mount", ctr], text=True, stdout=subprocess.PIPE, check=True)
+    mnt = p2.stdout.strip()
+    assert mnt
+    try:
+        yield Path(mnt)
+    finally:
+        run(["buildah", "unmount", ctr], check=True)
+
+
+def run(*args, **kwargs):
+    LOGGER.debug("%r", args)
+    return subprocess.run(*args, **kwargs)
 
 
 if __name__ == "__main__":
+    basicConfig(level="DEBUG")
     sys.exit(main(sys.argv))
 
 
